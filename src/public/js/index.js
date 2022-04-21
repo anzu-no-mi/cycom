23489fa255efc6e1544c23f3734d8da169edda35
Najah <najahmatsuo7@gmail.com>
2022-04-21 20:43:58 +0900

    js仮、CSS苦戦

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -7,3 +7,25 @@ function addContent() {
 
   elm.appendChild(newElm);
 }
+
+const images = ["images/img1.jpg", "images/img2.jpg"];
+let current = 0;
+
+
+ChangeScrean = (num) => {
+const img = document.getElementById('screan-img');
+
+  if(current + num >= 0 && current + num <= images.length - 1) {
+    current += num;
+    img.setAttribute('src', images[current]);
+  }else if(current + num == -1) {
+    current = images.length - 1;
+    img.setAttribute('src', images[current]);
+  }else {
+    current = 0;
+    img.setAttribute('src', images[current])
+  }
+}
+
+test = () => console.log(document.getElementById('screan-img').getAttribute('src'))
+
