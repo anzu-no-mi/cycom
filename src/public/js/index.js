b9875382d39ef1b1afc8e308c7b0b72c9a12a03f
Najah <najahmatsuo7@gmail.com>
2022-05-01 00:21:30 +0900

    main-scree関係系をクラス化、何名目のお知らせかを表示する所で苦戦

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,35 +1,59 @@
+// Q:クロージャに二つの関数をかえせるのか？とりま、わかりづらくなりそうなのでクラスにした
+class ScreenAction{
+  constructor(images,current) {
+    // お知らせ用の写真
 
+    this.images = images;
 
-// お知らせ用の写真
-const images = ["images/img1.jpg", "images/img2.jpg"];
-
-// 写真、何枚目か　カウンターでも使用するのでグローバルにしている。Q:globalなままでいいか？
-let current = 0;
-
-const  changeScrean = (num) => {
-  
-  const img = document.getElementById('screan-img');
-
-  // 通常時の遷移 (num＝1～N Nはお知らせの数)
-  if(current + num >= 0 && current + num <= images.length - 1) {
-    current += num;
-
-  // 最も右→最も左
-  }else if(current + num == -1) {
-    current = images.length - 1;
-
-  // 最も右→最も左
-  }else {
-    current = 0;
+    // 写真、何枚目か
+    this.current = current
   }
-  img.setAttribute('src', images[current]);
+  
+  // メソッド始まり
+  
+  changeScrean = (num) => {
+    
+    const img = document.getElementById('screan-img');
+
+    // 最も右のお知らせ→最も左のお知らせ 
+    if(this.current + num == -1) {
+      this.current = this.images.length - 1;
+
+    // 最も右のお知らせ→最も左のお知らせ
+    }else if(this.current + num == this.images.length) {
+      this.current = 0;
+
+    // 通常時の遷移 (num＝1～N Nはお知らせの数)
+    }else {
+      this.current += num;
+    }
+    img.setAttribute('src', this.images[this.current]);
+  }
+
+  // Q:関数の処理の順番について←「counter-〇」のため
+  imgCounter = ()=>{
+    const elm =document.getElementById("counter-" + this.current　+　1);
+    elm.classList.add("bg-primary");
+  }
+
+// TODO:追加の繰り返し処理。時間なかったので中断
+
+  generateCounter =　() => {
+    // お知らせの数
+    const n = this.images.length
+    // counterの要素（追加する親要素）
+    const elem = document.getElementById("img-counter")
+    // 追加する要素
+    const new_elem = document.createElement("div") 
+
+    for(let i = 0; i < 2; i++) {
+      elem.appendChild(new_elem)
+    }
+  }
+
 }
 
-どのようにN個に対応させるか
-const imgCounter = ()=>{
-  const elm =document.getElementById("counter-" + current);
-  elm.className = 'bg-primary';
-}
+testScreen = new ScreenAction(["images/img1.jpg", "images/img2.jpg"], 0)
 
 //イベントと関数がバインドされているかチェック用、後で消す
 test = () => console.log(document.getElementById('screan-img').getAttribute('src'))
