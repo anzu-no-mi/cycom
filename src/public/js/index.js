84cec6aa92e96adc8612355b51766d34a73a9805
Najah7 <90055003+Najah7@users.noreply.github.com>
2022-06-11 23:44:49 +0900

    トップ画面の開発 (#1)
    * fix buttun message
    
    * html仮
    
    * js仮、CSS苦戦
    
    * コメントアウトの追加
    
    * cssやり直し前
    
    * 前回のレビューの改善、Bootstrap導入
    
    * UIの修正（色は分かりやすくするため）、CSS削除
    
    * main-scree関係系をクラス化、何名目のお知らせかを表示する所で苦戦
    
    * swiper CDN
    
    * Swiper.js を index.html に適用. Style微調整
    
    * UIの修正
    
    * bodyのpaddingだけ修正
    
    * 縦のレイアウトを修正
    
    Co-authored-by: Tsubasa Nagata <25589480+ototonari@users.noreply.github.com>

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,9 +1,79 @@
+// Q:クロージャに二つの関数をかえせるのか？とりま、わかりづらくなりそうなのでクラスにした
+class ScreenAction {
+  constructor(images, current) {
+    // お知らせ用の写真
 
-function addContent() {
-  const elm = document.getElementById("content");
+    this.images = images;
 
-  const newElm = document.createElement("p");
-  newElm.innerHTML = "new element."
+    // 写真、何枚目か
+    this.current = current;
+  }
 
-  elm.appendChild(newElm);
+  // メソッド始まり
+
+  changeScrean = (num) => {
+    const img = document.getElementById("screan-img");
+
+    // 最も右のお知らせ→最も左のお知らせ
+    if (this.current + num == -1) {
+      this.current = this.images.length - 1;
+
+      // 最も右のお知らせ→最も左のお知らせ
+    } else if (this.current + num == this.images.length) {
+      this.current = 0;
+
+      // 通常時の遷移 (num＝1～N Nはお知らせの数)
+    } else {
+      this.current += num;
+    }
+    img.setAttribute("src", this.images[this.current]);
+  };
+
+  // Q:関数の処理の順番について←「counter-〇」のため
+  imgCounter = () => {
+    const elm = document.getElementById("counter-" + this.current + 1);
+    elm.classList.add("bg-primary");
+  };
+
+  // TODO:追加の繰り返し処理。時間なかったので中断
+
+  generateCounter = () => {
+    // お知らせの数
+    const n = this.images.length;
+    // counterの要素（追加する親要素）
+    const elem = document.getElementById("img-counter");
+    // 追加する要素
+    const new_elem = document.createElement("div");
+
+    for (let i = 0; i < 2; i++) {
+      elem.appendChild(new_elem);
+    }
+  };
 }
+
+
+// swiper object
+let swiper = null;
+const setupSwiper = () => {
+  swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+};
+
+window.addEventListener('load', (event) => {
+  console.log('ページが完全に読み込まれました');
+  setupSwiper();
+});
